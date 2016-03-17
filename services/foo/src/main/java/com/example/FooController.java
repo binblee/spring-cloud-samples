7b3016397bf0c104a99ed1a91a7a705cf008502f
Li Bin <binblee@users.noreply.github.com>
2016-03-17 22:29:42 +0800

    return foo message with instance specific data

--- services/foo/src/main/java/com/example/FooController.java
+++ services/foo/src/main/java/com/example/FooController.java
@@ -3,7 +3,11 @@ package com.example;
 import com.example.model.FooMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +25,20 @@ public class FooController {
     @Value("${message}")
     private String message;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/message")
-    FooMessage getMessage(){
+    public FooMessage getMessage(){
         FooMessage foomsg = new FooMessage();
-        foomsg.setName("Foo Service");
+        foomsg.setName(getLocalInstanceInfo());
         foomsg.setMessage(message);
         return foomsg;
+    }
+
+    private String getLocalInstanceInfo(){
+        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
+        return serviceInstance.getServiceId() + ":" + serviceInstance.getHost() +
+                ":" + serviceInstance.getPort();
     }
 }
