6a1a9639d5187e9333180e6e7b9331881870642e
Li Bin <binblee@users.noreply.github.com>
2016-03-19 22:25:39 +0800

    use loadbalancerclient with hystrix

--- services/foobar-hystrix/src/main/java/com/example/BarService.java
+++ services/foobar-hystrix/src/main/java/com/example/BarService.java
@@ -2,12 +2,9 @@ package com.example;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
 
 /**
  * Created by libin on 3/19/16.
@@ -17,15 +14,12 @@ import java.net.URI;
 public class BarService {
 
     @Autowired
-    private LoadBalancerClient loadBalancerClient;
+    @LoadBalanced
+    private RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "getMessageFallback")
     public String getMessage() {
-        ServiceInstance barInstance = loadBalancerClient.choose("bar");
-        URI barUri = barInstance.getUri();
-        RestTemplate restTemplate = new RestTemplate();
-        String url = barUri + "/message";
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject("http://bar/message", String.class);
     }
 
     public String getMessageFallback(){
