2dc9c48c0f189f3d4e17b11766ba6977ca72c9a2
libin <libin@libindeMacBook-Pro.local>
2016-04-26 11:49:21 +0800

    fix typo in src filename and improve dockerfile

--- discovery-server/src/main/java/com/example/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/DiscoveryServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoverServerApplication {
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoverServerApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 }
